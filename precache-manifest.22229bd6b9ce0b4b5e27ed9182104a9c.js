4357c2a64d9ba9f31a13779e10a152c73d0a7b48
Hyunsik Lee <hyunsiklee@bscswap.com>
2020-09-17 19:47:45 +0900

    Initial commit

---
+++ precache-manifest.22229bd6b9ce0b4b5e27ed9182104a9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dff0b48612d39d6de0c4491efefcd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3417c9733bb8daf6cfe",
+    "url": "/static/js/2.2cb9dc84.chunk.js"
+  },
+  {
+    "revision": "8b23cc64f7a6e5ec6ca5b5224be34b56",
+    "url": "/static/js/2.2cb9dc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4478814fb7a1146fda",
+    "url": "/static/js/main.270e6c62.chunk.js"
+  },
+  {
+    "revision": "7ba34e7e20b12d7e160c",
+    "url": "/static/js/runtime-main.1263790c.js"
+  },
+  {
+    "revision": "ef75c1df7562c0f95415792836c10636",
+    "url": "/static/media/bnb.ef75c1df.svg"
+  },
+  {
+    "revision": "5efc3eebdf8209b8a1f62d022f882039",
+    "url": "/static/media/logo.5efc3eeb.svg"
+  },
+  {
+    "revision": "29574b891d397dfa89a6b069db0acf52",
+    "url": "/static/media/logo_white.29574b89.svg"
+  },
+  {
+    "revision": "5551b60c3d59dbcbd470a76bdeef70a9",
+    "url": "/static/media/wordmark_white.5551b60c.svg"
+  }
+]);
